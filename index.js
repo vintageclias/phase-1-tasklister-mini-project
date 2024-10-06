f166bb93030d9c0c3b968e003bd09713c91f620f
44vin@44 <vintageclias@gmail.com>
2024-10-06 19:17:35 +0300

    mini-project

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form =document.getElementById('creat-task-form');
-    const taskList = document.getElementById('task');
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const taskDescription = document.getElementById('new-task-description').value;
+    const priority = document.getElementById('priority').value; 
+    
+    if (taskDescription) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
 
-        const taskDesciption = document.getElementById('new-task-descripyion').ariaValueMax;
-
-        if (taskDesciption) {
-            const taskItem = document.createElement('li');
-            taskItem.textContent = taskDesciption;
-            
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.addEventListener('click', () =>{
-                taskItem.remove();
-            });
-            taskItem.appendChild(deleteButton);
-            taskList.appendChild(taskItem);
-            form.requestFullscreen();
+      switch (priority) {
+          case 'high':
+            taskItem.style.color = 'red';
+            break;
+          case 'medium':
+            taskItem.style.color = 'orange';
+            break;
+          case 'low':
+            taskItem.style.color = 'green';
+            break;
         }
-    });
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        taskItem.remove();
+      });
+      
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+      
+      form.reset(); 
+    }
+  });
 });
